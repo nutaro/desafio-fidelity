e85102bb76cf02a988826ba7e9a2208cb6f941a5
nutaro <nutaro@protonmail.com>
2025-07-20 11:39:43 -0300

    using custom exceptions

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,12 @@
-class ToManyValuesException(Exception):
+class CustomException(Exception):
+    """ Common base class for all non-exit exceptions. """
+
+    def __init__(self, msg: str, *args, **kwargs):
+        self.msg = msg
+
+class ToManyValuesException(CustomException):
     pass
 
+
+class InvalidSearchException(CustomException):
+    pass
