860408b4d91bcc9b4902d9e801044b3844c15213
nutaro <nutaro@protonmail.com>
2025-07-20 13:30:39 -0300

    minor bugfix

--- src/decorator.py
+++ src/decorator.py
@@ -13,9 +13,10 @@ def time_it(function: Callable) -> Callable:
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         start_time = datetime.now(tz=ZoneInfo('America/Sao_Paulo'))
         logger.info(f'Executando {function.__name__} na {start_time}')
-        function(*args, **kwargs)
+        values = function(*args, **kwargs)
         stop_time = datetime.now(tz=ZoneInfo('America/Sao_Paulo'))
         logger.info(f'Finalizada {function.__name__} na {stop_time}')
         tempo_total = stop_time - start_time
         logger.info(f'A função {function.__name__} levou {tempo_total.total_seconds()} segundos')
+        return values
     return wrapper
